fd0fd9ba28c8ece2829c40a100eb8097dd9b899d
Jay Weisskopf <jay@jayschwa.net>
2014-04-19 18:57:44 -0400

    Redevelop GLFW 2 code
    It should still be compatible with the old version. Added a few
    things that were missing before, such as joystick support.

--- src/GLFW.jl
+++ src/GLFW.jl
@@ -10,18 +10,24 @@ if isempty(lib)
 	error("could not find GLFW library")
 end
 
-include("common.jl")
+# Retrieves the version of the GLFW library.
+function GetVersion()
+	major, minor, rev = Cint[0], Cint[0], Cint[0]
+	ccall( (:glfwGetVersion, lib), Void, (Ptr{Cint}, Ptr{Cint}, Ptr{Cint}), major, minor, rev)
+	(major[1], minor[1], rev[1])
+end
 
-const version = GetVersion()
-if version[1] == 2
+const (VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION) = GetVersion()
+
+if VERSION_MAJOR == 2
 	include("glfw2.jl")
-	info("loaded GLFW ", join(version, '.'))
+	info("loaded GLFW ", join(GetVersion(), '.'))
 	warn("GLFW 2 is outdated; consider upgrading to GLFW 3")
-elseif version[1] == 3
+elseif VERSION_MAJOR == 3
 	include("glfw3.jl")
 	info("loaded GLFW ", GetVersionString())
 else
-	error("GLFW ", join(version, '.'), " is not supported")
+	error("GLFW ", join(GetVersion(), '.'), " is not supported")
 end
 
 end
