2935d319f846287a40d55419a037a58201e59408
Jay Weisskopf <jay@jayschwa.net>
2014-05-16 03:28:38 -0400

    DRY out module initialization

--- src/GLFW.jl
+++ src/GLFW.jl
@@ -1,36 +1,33 @@
 module GLFW
 
-include("util.jl")
-
-const deps_path = Pkg.dir("GLFW/deps")
-if !(deps_path in DL_LOAD_PATH)
-	push!(DL_LOAD_PATH, deps_path)
-end
-
-const lib = find_library(["glfw3", "libglfw3", "glfw", "libglfw"])
+const lib = find_library(["glfw3", "libglfw3", "glfw", "libglfw"], [Pkg.dir("GLFW/deps")])
 if isempty(lib)
 	error("could not find GLFW library")
 end
 
-# Retrieves the version of the GLFW library.
+include("util.jl")
+
 function GetVersion()
 	major, minor, rev = Cint[0], Cint[0], Cint[0]
 	ccall( (:glfwGetVersion, lib), Void, (Ptr{Cint}, Ptr{Cint}, Ptr{Cint}), major, minor, rev)
 	VersionNumber(major[1], minor[1], rev[1])
 end
-
 const VERSION = GetVersion()
 
-if VERSION.major == 2
-	include("glfw2.jl")
-	info("loaded GLFW $VERSION")
-	warn("GLFW 2 is outdated, consider upgrading to GLFW 3")
-elseif VERSION.major == 3
-	include("glfw3.jl")
-	info("loaded GLFW ", GetVersionString())
-	SetErrorCallback((code, desc) -> error(bytestring(desc)))
-else
+try
+	include(string("glfw", VERSION.major, ".jl"))
+catch
 	error("GLFW $VERSION is not supported")
+end
+
+if isdefined(:SetErrorCallback)
+	SetErrorCallback((code, desc) -> error(bytestring(desc)))
+end
+
+info(string("loaded GLFW ", isdefined(:GetVersionString) ? GetVersionString() : VERSION, " from $lib"))
+
+if VERSION < VersionNumber(3)
+	warn("GLFW $VERSION is outdated, consider upgrading to a newer version")
 end
 
 end
