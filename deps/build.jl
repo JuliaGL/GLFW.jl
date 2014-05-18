59458b93482916c4b968fd412faae071c8a29d07
Jay Weisskopf <jay@jayschwa.net>
2014-05-18 01:54:22 -0400

    Build script tweaks

--- deps/build.jl
+++ deps/build.jl
@@ -1,26 +1,35 @@
+# version of library to download
 const version = VersionNumber(3, 0, 4)
+const glfw = "glfw-$version"
 
-map(mkpath, ("downloads", "src", "builds", "usr"))
+map(mkpath, ("downloads", "usr"))
 
+# download and compile the library from source
 @unix_only begin
-	const glfw = "glfw-$version"
-	tarball = "downloads/$glfw.tar.gz"
+	const tarball = "downloads/$glfw.tar.gz"
 	if !isfile(tarball)
 		download("https://github.com/glfw/glfw/archive/$version.tar.gz", tarball)
 	end
+	mkpath("src")
 	run(`tar xzf $tarball -C src`)
 	mkpath("builds/$glfw")
 	cd("builds/$glfw") do
-		run(`cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../../usr -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF ../../src/$glfw`)
+		options = map(x -> "-D$(x[1])=$(x[2])", [
+			"BUILD_SHARED_LIBS"     => "ON",
+			"CMAKE_INSTALL_PREFIX"  => "../../usr",
+			"GLFW_BUILD_DOCS"       => "OFF",
+			"GLFW_BUILD_EXAMPLES"   => "OFF",
+			"GLFW_BUILD_TESTS"      => "OFF"
+		])
+		run(`cmake $options ../../src/$glfw`)
 		run(`make install`)
 	end
 end
 
-# Automagically download and install a GLFW DLL for the poor souls using Windows
+# download a pre-compiled binary
 @windows_only begin
 	const dll = "glfw3.dll"
-	const archive = "glfw-$version.bin.WIN$WORD_SIZE.zip"
-
+	const archive = "$glfw.bin.WIN$WORD_SIZE.zip"
 	if !(dll in readdir() && archive in readdir())
 		info("Downloading GLFW $version binary for $WORD_SIZE-bit Windows")
 		download("https://github.com/jayschwa/glfw/releases/download/$version/$archive", archive)
