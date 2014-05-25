3855e661420bfbe77d914a4eed891fb3ef47a8e0
Yuri Vishnevsky <yurivish@gmail.com>
2014-05-24 22:57:58 -0400

    Fix simple.jl.
    - Added two C_NULL arguments to GLFW.CreateWindow; there's only one method defined and it expects monitor::Ptr{None} and share::Ptr{None} arguments
    - Check whether the return value of GLFW.WindowShouldClose is zero by == comparison rather than the boolean \!GLFW.WindowShouldClose(window), which throws an error because WindowShouldClose returns an int and ! is defined for Bools and BitArrays.

--- examples/simple.jl
+++ examples/simple.jl
@@ -4,13 +4,13 @@ import GLFW
 GLFW.Init()
 
 # Create a windowed mode window and its OpenGL context
-window = GLFW.CreateWindow(640, 480, "GLFW.jl")
+window = GLFW.CreateWindow(640, 480, "GLFW.jl", C_NULL, C_NULL)
 
 # Make the window's context current
 GLFW.MakeContextCurrent(window)
 
 # Loop until the user closes the window
-while !GLFW.WindowShouldClose(window)
+while GLFW.WindowShouldClose(window) == 0
 
 	# Render here
 
