9a5b1b09490194f80cd869fc70e59be5ee2a71ca
Jay Weisskopf <jay@jayschwa.net>
2015-05-19 02:18:51 -0400

    Add more callbacks and update wording in example

--- examples/callbacks.jl
+++ examples/callbacks.jl
@@ -4,21 +4,31 @@ GLFW.Init()
 window = GLFW.CreateWindow(800, 600, "GLFW Callback Test")
 GLFW.MakeContextCurrent(window)
 
+# Window callbacks
 GLFW.SetWindowCloseCallback(window, (_) -> println("window closed"))
+GLFW.SetWindowPosCallback(window, (_, x, y) -> println("window position: $x, $y"))
 GLFW.SetWindowSizeCallback(window, (_, w, h) -> println("window size: $h x $w"))
+GLFW.SetWindowFocusCallback(window, (_, focused) -> println("window focus: $focused"))
+GLFW.SetWindowIconifyCallback(window, (_, iconified) -> println("window iconify: $iconified"))
 
+# Input callbacks
+actiontext = Dict(
+	GLFW.RELEASE => "released",
+	GLFW.PRESS   => "pressed",
+	GLFW.REPEAT  => "repeats"
+)
 GLFW.SetKeyCallback(window, (_, key, scancode, action, mods) -> begin
 	if '!' <= key && key <= '~'
 		key = convert(Char, key)
 		key = string("'", key, "'")
 	end
-	println("key ", action == 1 ? "dn" : "up", ": $key")
+	println("key $key ", actiontext[action])
 end)
 
 GLFW.SetCharModsCallback(window, (_, c, mods) -> println("char: $c, mods: $mods"))
 GLFW.SetMouseButtonCallback(window, (_, button, action, mods) ->
-	println("mouse button ", action == 1 ? "dn" : "up", ": $button"))
-GLFW.SetCursorPosCallback(window, (_, x, y) -> println("cursor pos: $x, $y"))
+	println("mouse button $button ", actiontext[action]))
+GLFW.SetCursorPosCallback(window, (_, x, y) -> println("cursor: $x, $y"))
 GLFW.SetScrollCallback(window, (_, xoff, yoff) -> println("scroll: $xoff, $yoff"))
 
 GLFW.SetDropCallback(window, (_, paths) -> println(paths))
