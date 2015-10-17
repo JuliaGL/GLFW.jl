80a9fda17938eafa6f0a6c596a48ff46b2096690
Jay Weisskopf <jay@jayschwa.net>
2015-10-17 02:27:27 -0400

    Rename and rearrange variables in callback macro

--- src/util.jl
+++ src/util.jl
@@ -1,36 +1,41 @@
 # generate methods for wrapping and setting a callback
 macro callback(ex)
 	signature = ex.head == :call ? ex : ex.args[1]
-	name = signature.args[1]
+
+	name = string(signature.args[1], "Callback")
+	callback = gensym(name)
+	setter = symbol("Set", name)
+	libsetter = Expr(:quote, symbol("glfw", setter))
+	wrapper = symbol(name, "Wrapper")
+
 	ccall_args = signature.args[2:end]
 	ccall_arg_names = map(a -> a.args[1], ccall_args)
 	ccall_arg_types = Expr(:tuple, map(a -> a.args[2], ccall_args)...)
+	callback_arg_values = ex.head == :-> ? ex.args[2].args[2].args : ccall_arg_names
 
-	setfunc = symbol("Set", name, "Callback")
-	libfunc = Expr(:quote, symbol("glfw", setfunc))
-	wrapfunc = symbol(name, "CallbackWrapper") # wrapper function that meets cfunction requirements
-	jcbfunc = gensym(string(name, "Callback")) # Julia callback function called by wrapper function
-	jcbfunc_arg_values = ex.head == :-> ? ex.args[2].args[2].args : ccall_arg_names
-
-	window_callback = length(ccall_arg_types.args) >= 1 && ccall_arg_types.args[1] == :Window
-	win_arg = window_callback ? (ccall_args[1],) : ()
-	win_name = window_callback ? (ccall_arg_names[1],) : ()
-	win_type = window_callback ? (ccall_arg_types.args[1],) : ()
+	is_window_callback = length(ccall_arg_types.args) >= 1 && ccall_arg_types.args[1] == :Window
+	win_arg = is_window_callback ? (ccall_args[1],) : ()
+	win_name = is_window_callback ? (ccall_arg_names[1],) : ()
+	win_type = is_window_callback ? (ccall_arg_types.args[1],) : ()
 
 	ex = quote
-		function $setfunc($(win_arg...), callback::Function)
-			cfun = (isgeneric(callback) && method_exists(callback, $ccall_arg_types)) ? callback : $wrapfunc
-			$setfunc($(win_name...), cfunction(cfun, Void, $ccall_arg_types), callback)
+		# Set the callback to a Julia function
+		function $setter($(win_arg...), callback::Function)
+			cfun = (isgeneric(callback) && method_exists(callback, $ccall_arg_types)) ? callback : $wrapper
+			$setter($(win_name...), cfunction(cfun, Void, $ccall_arg_types), callback)
 		end
 
-		function $setfunc($(win_arg...), cfunptr::Ptr{Void}, jfunref=nothing)
-			global $jcbfunc = jfunref # prevent Julia function from being garbage-collected
-			ccall( ($libfunc, lib), Void, ($(win_type...), Ptr{Void}), $(win_name...), cfunptr)
+		# Set the callback to a C function pointer
+		function $setter($(win_arg...), cfunptr::Ptr{Void}, jlfunref=nothing)
+			global $callback = jlfunref # prevent callback from being garbage-collected
+			ccall( ($libsetter, lib), Void, ($(win_type...), Ptr{Void}), $(win_name...), cfunptr)
 		end
 
-		$setfunc($(win_arg...), ::Void) = $setfunc($(win_name...), C_NULL)
+		# Remove the current callback
+		$setter($(win_arg...), ::Void) = $setter($(win_name...), C_NULL)
 
-		$wrapfunc($(ccall_args...)) = ($jcbfunc($(jcbfunc_arg_values...)); return nothing)
+		# Julia callback wrapper that can be passed to `cfunction`
+		$wrapper($(ccall_args...)) = ($callback($(callback_arg_values...)); return nothing)
 	end
 	esc(ex)
 end
