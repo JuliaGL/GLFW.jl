378febddb1cf1c34cda4f9f7e4faaea9a36a40f1
Jay Weisskopf <jay@jayschwa.net>
2015-10-26 22:42:09 -0400

    Fix call to glClear in multiwindow example
    Attempting to get the address of glClear won't work until a context is
    current, and each context might have a different address!

--- examples/multiwindow.jl
+++ examples/multiwindow.jl
@@ -1,25 +1,30 @@
 import GLFW
 
+# Note: This was written as a quick test for multiwindow support.
+# It is not a clean example (yet) of the proper way to do it.
+
 windows = []
+glClear = Dict{GLFW.Window, Function}()
 for i in 1:3
 	name = "Window $i"
 	window = GLFW.CreateWindow(640, 480, name)
+	GLFW.MakeContextCurrent(window)
 	GLFW.SetMouseButtonCallback(window, (_, button, action, mods) -> begin
 		if action == GLFW.PRESS
 			println(name)
 		end
 	end)
+	glClearFunc() = ccall(@eval(GLFW.GetProcAddress("glClear")), Void, (Cuint,), 0x00004000)
+	glClear[window] = glClearFunc 
 	push!(windows, window)
 end
-
-glClear() = ccall(@eval(GLFW.GetProcAddress("glClear")), Void, (Cuint,), 0x00004000)
 
 gc() # Force garbage collection so that improper reference management is more apparent via crashes
 
 while !any(GLFW.WindowShouldClose, windows)
 	for window in windows
 		GLFW.MakeContextCurrent(window)
-		glClear()
+		glClear[window]()
 		GLFW.SwapBuffers(window)
 	end
 	GLFW.WaitEvents()
