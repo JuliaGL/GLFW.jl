2d7af7f6acaf9b7451de28fb34bf9698bc213a8b
SimonDanisch <sdanisch@gmail.com>
2016-03-04 15:04:08 +0100

    try xdummy

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,1 @@
-travis = get(ENV, "TRAVIS", "") == "true"
-
-try
-	# try...catch introduces a new scope, so we need to eval the import
-	# into the Main module scope.
-	eval(Main, :(import GLFW))
-catch e
-	if travis && contains(e.msg, "/dev/input: No such file or directory")
-		warn(e.msg)
-	else
-		rethrow()
-	end
-end
+using GLFW
