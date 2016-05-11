015c8d201c96e9b1bb81480a6820d134c0007984
Jay Weisskopf <jay@jayschwa.net>
2016-05-11 17:36:42 -0400

    Fix OS X Travis builds

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,7 @@
 travis = get(ENV, "TRAVIS", "") == "true"
 
-try
-	# try...catch introduces a new scope, so we need to eval the import
-	# into the Main module scope.
-	eval(Main, :(import GLFW))
-        include("windowclose.jl")
-catch e
-	if travis && contains(e.msg, "/dev/input: No such file or directory")
-		warn(e.msg)
-	else
-		rethrow()
-	end
+import GLFW
+
+if !travis
+	include("windowclose.jl")
 end
