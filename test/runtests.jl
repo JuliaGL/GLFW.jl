6a73912a7abc9a20d5421da34a429aecbeac568e
SimonDanisch <sdanisch@gmail.com>
2017-03-10 14:29:43 +0100

    fix 0.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ close(io_rd)
 err_stream = redirect_stderr(ORIGINAL_IO)
 
 # Contains, to be resilient against spliced in color commands
-@test contains(warning, """WARNING: GLFW reports the following error: X11: RandR gamma ramp support seems broken.
+@test contains(warning, """GLFW reports the following error: X11: RandR gamma ramp support seems broken.
 This can be ignored on a headless system.""")
 @test_throws GLFW.GLFWError GLFW.handle_error(Cint(GLFW.PLATFORM_ERROR), "test")
 
