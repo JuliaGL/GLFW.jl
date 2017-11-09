c4a9d2cffcfd5c151f546f9162f0f24947876857
SimonDanisch <sdanisch@gmail.com>
2017-11-09 15:48:04 +0100

    didn't save file

--- src/GLFW.jl
+++ src/GLFW.jl
@@ -18,28 +18,8 @@ else
 	error("GLFW $libversion is not supported")
 end
 
-<<<<<<< HEAD
-function handle_error(int_code, description)
-	code = ErrorCode(int_code)
-	if code == PLATFORM_ERROR && (
-			contains(description, "Failed to get display service port iterator") ||
-			contains(description, "Failed to retrieve display name") ||
-			contains(description, "RandR gamma ramp support seems broken") ||
-			contains(description, "RandR monitor support seems broken") ||
-			contains(description, "Failed to watch for joystick connections in") ||
-			contains(description, "Failed to open joystick device directory")
-		)
-		# Workaround: downgrade Mac display name error to warning
-		# https://github.com/glfw/glfw/issues/958
-		#warn("GLFW reports the following error: $description.\nThis can be ignored on a headless system.")
-	else
-		throw(GLFWError(code, description))
-	end
-end
-=======
 pusherror!(collection) = (code, description) -> push!(collection, GLFWError(code, description))
 throwerror(code, description) = throw(GLFWError(code, description))
->>>>>>> master
 
 function __init__()
 	initialized = false
