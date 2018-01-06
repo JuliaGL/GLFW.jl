5b22047ba5945b92a26fe69e29b606d2bafb5bdc
Jay Weisskopf <jay@jayschwa.net>
2018-01-06 02:29:18 -0500

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,11 @@ import GLFW
 using Base.Test
 
 # GLFWError uses enum for known error code
-@test isa(GLFWError(GLFW.NOT_INITIALIZED, "").code, GLFW.ErrorCode)
-@test GLFWError(0x00010001, "").code == GLFW.NOT_INITIALIZED
+@test isa(GLFW.GLFWError(GLFW.NOT_INITIALIZED, "").code, GLFW.ErrorCode)
+@test GLFW.GLFWError(0x00010001, "").code == GLFW.NOT_INITIALIZED
 
 # GLFWError uses integer for unrecognized error code
-@test isa(GLFWError(0xDEADBEEF, "").code, Integer)
+@test isa(GLFW.GLFWError(0xDEADBEEF, "").code, Integer)
 
 if !haskey(ENV, "TRAVIS")
 	include("windowclose.jl")
