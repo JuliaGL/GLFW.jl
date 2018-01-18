d7a479e2b2c3a33bb305e3bcf7d3a199d9eb48fb
Jay Weisskopf <jay@jayschwa.net>
2018-01-17 23:41:28 -0500

    Checkpoint

--- src/callback.jl
+++ src/callback.jl
@@ -7,9 +7,21 @@ macro callback(ex)
 	ref = gensym()
 	declare_ref = :($ref = Ref{Function}(undef))
 	ref = :($ref[])
+
+	# Decompose expression
+	warp = ex.head == :->
+	name_with_params = warp ? ex.args[1] : ex  # Foo(x::T1, y::T2, etc)
+	name = string(name_with_params.args[1])    # Foo
+	params = name_with_params.args[2:end]      #    (x::T1, y::T2, etc)
+	transformed_args = if warp
+		ex.args[2].args[2].args                # Everything after ->
+	else
+		map(paramname, params)                 #    (x,     y,     etc)
+	end
+
 	esc(quote
 		$declare_ref
-		$(callback_expr(ex, ref))
+		$(callbackexpr(name, params, ref, transformed_args=transformed_args))
 	end)
 end
 
@@ -17,58 +29,63 @@ macro windowcallback(ex)
 	_window_callbacks_len[] += 1
 	idx = _window_callbacks_len[]
 	ref = :(_window_callbacks[window][$idx])
-	esc(callback_expr(ex, ref, :window, :Window))
-end
 
-# Generate functions for wrapping and setting a callback
-function callback_expr(ex, jl_callback_ref, handle_arg=nothing, handle_type=nothing)
 	# Decompose expression
 	warp = ex.head == :->
 	name_with_params = warp ? ex.args[1] : ex  # Foo(x::T1, y::T2, etc)
 	name = string(name_with_params.args[1])    # Foo
 	params = name_with_params.args[2:end]      #    (x::T1, y::T2, etc)
-	jl_callback_args = if warp
+	transformed_args = if warp
 		ex.args[2].args[2].args                # Everything after ->
 	else
-		map(argname, params)                   #    (x,     y,     etc)
+		map(paramname, params)                 #    (x,     y,     etc)
 	end
-	callback_types = Expr(:curly, :Tuple, map(argtype, params)...)
 
-	# Construct function name symbols
+	esc(callbackexpr(name, params, ref, setter_params=[:(window::Window)], transformed_args=transformed_args))
+end
+
+function callbackexpr(
+	name,
+	callback_params,
+	callback_var_ex;
+	setter_params=[],
+	transformed_args=map(paramname, callback_params),
+)
+	# Construct function names
 	setter = Symbol("Set", name, "Callback")          # SetFooCallback
 	libsetter = Expr(:quote, Symbol("glfw", setter))  # glfwSetFooCallback
-	wrapper = Symbol(string('_', name, "Wrapper"))    # _FooCallbackWrapper
+	wrapper = Symbol('_', name, "CallbackWrapper")    # _FooCallbackWrapper
 
-	# Convert to iterables that can be splatted into expression
-	handle_arg = filter(notnothing, [handle_arg])
-	handle_type = filter(notnothing, [handle_type])
+	# Separate names and types of parameters
+	callback_param_types = Expr(:curly, :Tuple, map(paramtype, callback_params)...)
+	setter_param_names = map(paramname, setter_params)
+	setter_param_types = map(paramtype, setter_params)
 
 	quote
 		# Set the callback function
-		function $setter($(handle_arg...), callback::Function)
-			old_jl_callback = $jl_callback_ref
-			$jl_callback_ref = callback # Prevent Julia function from being garbage-collected
-			cfunptr = cfunction($wrapper, Cvoid, $callback_types)
-			old_cfunptr = ccall( ($libsetter, lib), Ptr{Cvoid}, ($(handle_type...), Ptr{Cvoid}), $(handle_arg...), cfunptr)
-			return old_cfunptr == C_NULL ? nothing : old_jl_callback
+		function $setter($(setter_param_names...), callback::Function)
+			old_callback = $callback_var_ex
+			$callback_var_ex = callback # Prevent Julia function from being garbage-collected
+			cfunptr = cfunction($wrapper, Cvoid, $callback_param_types)
+			old_cfunptr = ccall( ($libsetter, lib), Ptr{Cvoid}, ($(setter_param_types...), Ptr{Cvoid}), $(setter_param_names...), cfunptr)
+			return old_cfunptr == C_NULL ? nothing : old_callback
 		end
 
 		# Unset the callback function
-		function $setter($(handle_arg...), ::Nothing)
-			old_cfunptr = ccall( ($libsetter, lib), Ptr{Cvoid}, ($(handle_type...), Ptr{Cvoid}), $(handle_arg...), C_NULL)
-			old_jl_callback = $jl_callback_ref
-			$jl_callback_ref = undef # Allow former callback function to be garbage-collected
-			return old_cfunptr == C_NULL ? nothing : old_jl_callback
+		function $setter($(setter_param_names...), ::Nothing)
+			old_cfunptr = ccall( ($libsetter, lib), Ptr{Cvoid}, ($(setter_param_types...), Ptr{Cvoid}), $(setter_param_names...), C_NULL)
+			old_callback = $callback_var_ex
+			$callback_var_ex = undef # Allow former callback function to be garbage-collected
+			return old_cfunptr == C_NULL ? nothing : old_callback
 		end
 
 		# Julia callback wrapper that can be passed to `cfunction`
-		$wrapper($(params...)) = ($jl_callback_ref($(jl_callback_args...)); return nothing)
+		$wrapper($(callback_params...)) = ($callback_var_ex($(transformed_args...)); return nothing)
 	end
 end
 
 # Helper functions for the callback macro
-argname(ex) = ex.args[1]
-argtype(ex) = ex.args[2]
-iswindow(ex) = argtype(ex) == :Window
+paramname(ex) = ex.args[1]
+paramtype(ex) = ex.args[2]
 notnothing(a) = a != nothing
 undef(any...) = throw(UndefRefError())
