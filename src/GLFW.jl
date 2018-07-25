9ccc763fb4c5be8a2f1a2533695b0332ae8456f0
SimonDanisch <sdanisch@gmail.com>
2018-07-25 20:31:15 +0200

    try getting rid of precompilation segfault

--- src/GLFW.jl
+++ src/GLFW.jl
@@ -10,24 +10,26 @@ function GetVersion()
 	ccall( (:glfwGetVersion, lib), Cvoid, (Ref{Cint}, Ref{Cint}, Ref{Cint}), major, minor, rev)
 	VersionNumber(major[], minor[], rev[])
 end
-const libversion = GetVersion()
 
-if libversion.major == 3
-	include("callback.jl")
-	include("glfw3.jl")
-	include("vulkan.jl")
-	if isdefined(Base, :getproperty) && isdefined(Base, :setproperty!)
-		# Julia 0.7 supports property overloading
-		# TODO for Julia upgrade: remove feature guard
-		include("monitor_properties.jl")
-	end
-else
-	error("GLFW $libversion is not supported")
+include("callback.jl")
+include("glfw3.jl")
+include("vulkan.jl")
+
+if isdefined(Base, :getproperty) && isdefined(Base, :setproperty!)
+	# Julia 0.7 supports property overloading
+	# TODO for Julia upgrade: remove feature guard
+	include("monitor_properties.jl")
 end
 
 function __init__()
 	initialized = false
+	check_deps()
 
+	libversion = GetVersion()
+
+	if libversion.major != 3
+		error("GLFW version $libversion not supported")
+	end
 	# Save errors that occur during initialization
 	errors = Vector{Exception}()
 	SetErrorCallback(err -> push!(errors, err))
