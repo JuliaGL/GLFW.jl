316ad67c0f1751b02f2bf18649e2b45e3e8c5012
Jay Weisskopf <jay@jayschwa.net>
2018-08-12 12:21:00 -0400

    Add some missing functionality and TODOs

--- src/vulkan.jl
+++ src/vulkan.jl
@@ -30,16 +30,6 @@ function GetRequiredInstanceExtensions()
 end
 
 """
-    CreateWindowSurface(instance, window, allocator=C_NULL)
-Create a Vulkan surface for the specified window.
-"""
-function CreateWindowSurface(instance, window, allocator=C_NULL)
-    surface = Ref{VkSurfaceKHR}(C_NULL)
-    ccall((:glfwCreateWindowSurface, lib), VkResult, (VkInstance, Window, Ptr{VkAllocationCallbacks}, Ref{VkSurfaceKHR}), instance, window, allocator, surface)
-    return surface[]
-end
-
-"""
     GetInstanceProcAddress(instance, procname) -> funcptr
 Return the address of the specified Vulkan core or extension function for the specified instance.
 `funcptr` can be used directly as the first argument of `ccall`: ccall(funcptr, ...).
@@ -51,3 +41,13 @@ GetInstanceProcAddress(instance, procname) = ccall((:glfwGetInstanceProcAddress,
 Return whether the specified queue family of the specified physical device supports presentation to the platform GLFW was built for.
 """
 GetPhysicalDevicePresentationSupport(instance, device, queuefamily) = Bool(ccall((:glfwGetPhysicalDevicePresentationSupport, lib), Cint, (VkInstance, VkPhysicalDevice, Cuint), instance, device, queuefamily))
+
+"""
+    CreateWindowSurface(instance, window, allocator=C_NULL)
+Create a Vulkan surface for the specified window.
+"""
+function CreateWindowSurface(instance, window, allocator=C_NULL)
+    surface = Ref{VkSurfaceKHR}(C_NULL)
+    ccall((:glfwCreateWindowSurface, lib), VkResult, (VkInstance, Window, Ptr{VkAllocationCallbacks}, Ref{VkSurfaceKHR}), instance, window, allocator, surface)
+    return surface[]
+end
