7c90c95273e24468f4b59d138a15a0182b3dc00e
Simon Danisch <sdanisch@gmail.com>
2019-04-24 14:36:03 +0200

    use binaryprovider hybrid cmake solution

--- src/vulkan.jl
+++ src/vulkan.jl
@@ -17,7 +17,7 @@ end
     VulkanSupported()
 Return whether the Vulkan loader has been found.
 """
-VulkanSupported() = Bool(ccall((:glfwVulkanSupported, lib), Cint, ()))
+VulkanSupported() = Bool(ccall((:glfwVulkanSupported, libglfw), Cint, ()))
 
 """
     GetRequiredInstanceExtensions()
@@ -25,7 +25,7 @@ Return a vector of names of Vulkan instance extensions.
 """
 function GetRequiredInstanceExtensions()
     count = Ref{Cuint}(0)
-    ptr = ccall((:glfwGetRequiredInstanceExtensions, lib), Ptr{Ptr{Cchar}}, (Ref{Cuint},), count)
+    ptr = ccall((:glfwGetRequiredInstanceExtensions, libglfw), Ptr{Ptr{Cchar}}, (Ref{Cuint},), count)
     return unsafe_string.(unsafe_wrap(Array, ptr, count[]))
 end
 
@@ -34,13 +34,13 @@ end
 Return the address of the specified Vulkan core or extension function for the specified instance.
 `funcptr` can be used directly as the first argument of `ccall`: ccall(funcptr, ...).
 """
-GetInstanceProcAddress(instance, procname) = ccall((:glfwGetInstanceProcAddress, lib), Ptr{Cvoid}, (VkInstance, Cstring), instance, procname)
+GetInstanceProcAddress(instance, procname) = ccall((:glfwGetInstanceProcAddress, libglfw), Ptr{Cvoid}, (VkInstance, Cstring), instance, procname)
 
 """
     GetPhysicalDevicePresentationSupport(instance, device, queuefamily)
 Return whether the specified queue family of the specified physical device supports presentation to the platform GLFW was built for.
 """
-GetPhysicalDevicePresentationSupport(instance, device, queuefamily) = Bool(ccall((:glfwGetPhysicalDevicePresentationSupport, lib), Cint, (VkInstance, VkPhysicalDevice, Cuint), instance, device, queuefamily))
+GetPhysicalDevicePresentationSupport(instance, device, queuefamily) = Bool(ccall((:glfwGetPhysicalDevicePresentationSupport, libglfw), Cint, (VkInstance, VkPhysicalDevice, Cuint), instance, device, queuefamily))
 
 """
     CreateWindowSurface(instance, window, allocator=C_NULL)
@@ -48,6 +48,6 @@ Create a Vulkan surface for the specified window.
 """
 function CreateWindowSurface(instance, window, allocator=C_NULL)
     surface = Ref{VkSurfaceKHR}(C_NULL)
-    ccall((:glfwCreateWindowSurface, lib), VkResult, (VkInstance, Window, Ptr{VkAllocationCallbacks}, Ref{VkSurfaceKHR}), instance, window, allocator, surface)
+    ccall((:glfwCreateWindowSurface, libglfw), VkResult, (VkInstance, Window, Ptr{VkAllocationCallbacks}, Ref{VkSurfaceKHR}), instance, window, allocator, surface)
     return surface[]
 end
