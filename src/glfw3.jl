JuliaGL/GLFW.jl

commit a50b6520c62b1e6999abfa0879e0f59d982e46cf

Replace `MonitorProperties` with property overloading (#128)

src/glfw3.jl
@@ -386,39 +386,6 @@ struct GLFWError <: Exception
 end
 Base.showerror(io::IO, e::GLFWError) = print(io, "GLFWError ($(e.code)): ", e.description)
 
-# From GLWindow.jl/types.jl
-struct MonitorProperties
-	name::String
-	isprimary::Bool
-	position::NTuple{2, Int}
-	physicalsize::NTuple{2, Int}
-	videomode::VidMode
-	videomode_supported::Vector{VidMode}
-	dpi::NTuple{2, Float64}
-	monitor::Monitor
-end
-
-function MonitorProperties(monitor::Monitor)
-	name = GetMonitorName(monitor)
-	isprimary = GetPrimaryMonitor() == monitor
-	position = GetMonitorPos(monitor)
-	physicalsize = GetMonitorPhysicalSize(monitor)
-	videomode = GetVideoMode(monitor)
-	sfactor = is_apple() ? 2.0 : 1.0
-	dpi = (videomode.width * 25.4, videomode.height * 25.4) * sfactor ./ physicalsize
-	videomode_supported = GetVideoModes(monitor)
-
-	MonitorProperties(name, isprimary, position, physicalsize, videomode, videomode_supported, dpi, monitor)
-end
-
-# From GLWindow.jl/core.jl
-function Base.show(io::IO, m::MonitorProperties)
-	println(io, "name: ", m.name)
-	println(io, "physicalsize: ",  m.physicalsize[1], "x", m.physicalsize[2])
-	println(io, "resolution: ", m.videomode.width, "x", m.videomode.height)
-	println(io, "dpi: ", m.dpi[1], "x", m.dpi[2])
-end
-
 #************************************************************************
 # GLFW API functions
 #************************************************************************

src/GLFW.jl
@@ -15,6 +15,11 @@ const libversion = GetVersion()
 if libversion.major == 3
 	include("callback.jl")
 	include("glfw3.jl")
+	if isdefined(Base, :getproperty) && isdefined(Base, :setproperty!)
+		# Julia 0.7 supports property overloading
+		# TODO for Julia upgrade: remove feature guard
+		include("monitor_properties.jl")
+	end
 else
 	error("GLFW $libversion is not supported")
 end

src/monitor_properties.jl
@@ -1,0 +1,18 @@
+function Base.getproperty(monitor::Monitor, name::Symbol)
+	# TODO for GLFW 3.3: contentscale is GetMonitorContentScale
+	if name == :isprimary
+		GetPrimaryMonitor() == monitor
+	elseif name == :name
+		GetMonitorName(monitor)
+	elseif name == :physicalsize
+		GetMonitorPhysicalSize(monitor)
+	elseif name == :position
+		GetMonitorPos(monitor)
+	elseif name == :supported_videomodes
+		GetVideoModes(monitor)
+	elseif name == :videomode
+		GetVideoMode(monitor)
+	else
+		getfield(monitor, name)
+	end
+end

commit ecab5175afe7beb78b16ea48ffc30cd47db87592

Fix #193: Remove use of deleted `MonitorProperties`
Bug introduced in PR #128.

Project.toml
@@ -1,7 +1,7 @@
 name = "GLFW"
 uuid = "f7f18e0c-5ee9-5ccd-a5bf-e8befd85ed98"
 author = ["Jay Petacat <jay@jayschwa.net>", "Simon Danisch <sdanisch@gmail.com>"]
-version = "3.2.0"
+version = "3.2.1"
 
 [deps]
 GLFW_jll = "0656b61e-2033-5cc2-a64a-77c0f6c09b89"

src/glfw3.jl
@@ -668,10 +668,8 @@ GetProcAddress(procname::AbstractString) = ccall((:glfwGetProcAddress, libglfw),
 Returns the monitor resolution of the primary monitor.
 """
 function primarymonitorresolution()
-	props = MonitorProperties(GetPrimaryMonitor())
-	w,h = props.videomode.width, props.videomode.height
-	# Vec(Int(w),Int(h))
-	(Int(w),Int(h))
+	m = GetPrimaryMonitor().videomode
+	(Int(m.width), Int(m.height))
 end
 
 #Came from GLWindow.jl/screen.jl